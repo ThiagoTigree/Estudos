3d26c9f465928e98b6a4f37f4c10ccd0dc95e618
Tigre <thiago.mendes3101@gmail.com>
2026-08-24 10:46:48 -0400

    commitando o exercicio 2

---
+++ exercicios/exercicio2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+#include<locale.h>
+int main()
+{
+//aumento de 30% para funcionários com o salario(entrada) inferior a 500 reais
+    setlocale(LC_ALL, "Portuguese");
+   float salini,salajust;
+
+   printf("Insira o valor do seu salário:  ");
+   scanf("%f",&salini);
+
+   if(salini <= 500)
+   {
+   salajust = salini * 1.30;
+   printf("Parabéns, seu novo salário é de %.2f !!",salajust);
+   }else if(salini > 500)
+   {
+   printf("Você não tem direito ao reajust salarial!!! ");
+   } return 0;
+}
