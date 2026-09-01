3a9a723edd7a9ea94acc077530be7fdb4c35ab1c
tigre <mestrebangal@gmail.com>
2026-09-01 14:37:56 -0400

    commit do exercicio do classroom

---
+++ emmerson pilatra/med arit de um conjunto de numeros pares.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<locale.h>
+int main(void)
+{
+    int vetor,soma = 0;
+    float ma = 0;
+    printf("Digite o tamanho do conjunto de numeros : ");
+    scanf("%d",&vetor);
+
+    int numpar[vetor];
+
+    for(int i = 0;i<vetor;i++)//loop para pegar o numero de cada um dos numeros contidos no vetor
+    {
+        printf("Insira o numero par %d : ",i + 1);
+        scanf("%d",&numpar[i]);
+        soma += numpar[i];// acumula os numeros obtidos para que nos possamos dividir depois
+    }
+
+    ma = (float)soma / vetor;
+
+    printf("O valor da media aritmetica eh de : %.2f ",ma);
+    return 0;
+}
